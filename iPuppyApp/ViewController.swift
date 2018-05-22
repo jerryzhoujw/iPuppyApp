1af218d01ae96be83d67c4208cd1f61b8b668964
jerryzhoujw <jerryzhoujw@gmail.com>
2018-05-22 19:09:02 +0800

    add test  but failed as comment

--- iPuppyApp/ViewController.swift
+++ iPuppyApp/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import cat_names
+import dog_names
+import mouse_names
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        let a = CatName.Cat1
+        let a1 = cat_namesVersionNumber
+        
+        let b = DogName // Use of unresolved identifier 'DogName'
+        let b1 = dog_namesVersionNumber // Use of unresolved identifier 'dog_namesVersionNumber'
 
+        let c = MouseName // Use of unresolved identifier 'MouseName'
+        let c1 = mouse_namesVersionNumber
+        
+        print(a)
+        print(a1)
+        print(c1)
+        print("Show cat1, cat2 above")
+    }
 
 }
 
